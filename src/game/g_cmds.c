a599c9b14b3125f242547b0091af9b2afd8f1898
Nicolas Boutet <amd3002@gmail.com>
2012-02-23 01:08:53 +0100

    Fixed callvote exploit

--- src/game/g_cmds.c
+++ src/game/g_cmds.c
@@ -1683,7 +1683,12 @@ qboolean Cmd_CallVote_f( gentity_t *ent, unsigned int dwCommand, qboolean fRefCo
 	trap_Argv( 1, arg1, sizeof( arg1 ) );
 	trap_Argv( 2, arg2, sizeof( arg2 ) );
 
-	if ( strchr( arg1, ';' ) || strchr( arg2, ';' ) ) {
+
+	// Nico, bugfix: callvote exploit fixed
+	// http://aluigi.freeforums.org/quake3-engine-callvote-bug-t686.html
+	if (strchr( arg1, ';' ) || strchr( arg2, ';' ) ||
+		strchr( arg1, '\r' ) || strchr( arg2, '\r' ) ||
+		strchr( arg1, '\n' ) || strchr( arg2, '\n' )) {
 		char *strCmdBase = ( !fRefCommand ) ? "vote" : "ref command";
 
 		G_refPrintf( ent, "Invalid %s string.", strCmdBase );
