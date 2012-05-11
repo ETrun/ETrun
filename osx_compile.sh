7aa9ee379f77931550cd6b91b1b52db073b0bc5c
Nicolas Boutet <amd3002@gmail.com>
2012-05-11 17:03:56 +0200

    Mod working on OSX, aded OSX test scripts

--- osx_compile.sh
+++ osx_compile.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
 BUILD_DIR=_build
+QAGAME_LIB=qagame_mac
+API_LIB=api_mac
+CGAME_LIB=cgame_mac
+UI_LIB=ui_mac
 
 # Clean
 make distclean
+
+rm -f $BUILD_DIR/*_mac
+rm -rf $BUILD_DIR/*_mac.bundle
 
 # Compile for linux
 ./configure CFLAGS="-m32"
@@ -17,7 +24,13 @@ strip -x src/cgame/.libs/libcgame.dylib
 strip -x src/game/.libs/libgame.dylib
 strip -x src/ui/.libs/libui.dylib
 strip -x src/APImodule/.libs/libapi.dylib
-mv src/cgame/.libs/libcgame.dylib $BUILD_DIR/cgame_mac
-mv src/game/.libs/libgame.dylib $BUILD_DIR/qagame_mac
-mv src/ui/.libs/libui.dylib $BUILD_DIR/ui_mac
-mv src/APImodule/.libs/libapi.dylib $BUILD_DIR/api_mac
+mv src/cgame/.libs/libcgame.dylib $BUILD_DIR/$CGAME_LIB
+mv src/game/.libs/libgame.dylib $BUILD_DIR/$QAGAME_LIB
+mv src/ui/.libs/libui.dylib $BUILD_DIR/$UI_LIB
+mv src/APImodule/.libs/libapi.dylib $BUILD_DIR/$API_LIB
+
+cd $BUILD_DIR
+../makebundle.sh qagame
+../makebundle.sh cgame
+../makebundle.sh ui
+cd ..
---
+++ test/osx/test_api.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+basepath="/Applications/ET"
+path_to_game_exec="$basepath/ET.app/Contents/MacOS/ET"
+
+mod_name="etrun"
+pk3_name="etrun.pk3"
+
+apimodule_name='api_mac'
+qagame_name='qagame_mac'
+cgame_name='cgame_mac'
+ui_name='ui_mac'
+
+path_to_api="$basepath/$mod_name/$apimodule_name"
+
+cp -rf _build/$cgame_name $mod_name/
+cp -rf _build/$ui_name $mod_name/
+
+rm -f $pk3name
+cd etrun
+zip -r ../$pk3_name *
+rm -rf $cgame_name
+rm -rf $ui_name
+rm -rf $qagame_name
+rm -rf $apimodule_name
+cd ..
+mkdir "$basepath/$mod_name"
+cp -f $pk3_name "$basepath/$mod_name"
+rm -f $pk3_name
+cp -rf _build/$qagame_name "$basepath/$mod_name"
+cp -rf _build/$apimodule_name "$basepath/$mod_name"
+read -p "Press ENTER to start game..."
+"$path_to_game_exec" +set fs_game $mod_name +set dedicated 2 +map goldrush +set g_useAPI 1 +set g_APImodulePath "$path_to_api"
