8ed463b1b65b0539224892f24e5ae53bb5abf92f
Nicolas Boutet <amd3002@gmail.com>
2013-05-05 00:07:17 +0200

    Added separate physics test scripts

---
+++ test/win32/test_api_developer_ap.bat
@@ -0,0 +1,21 @@
+cd ../../
+xcopy /Y build\cgame_mp_x86.dll etrun\
+xcopy /Y build\ui_mp_x86.dll etrun\
+del /Q etrun.pk3
+cd etrun
+..\test\win32\zip.exe -r ..\etrun.pk3 *
+del /Q *.dll
+cd ..\
+xcopy /Y etrun.pk3 C:\Users\Nicolas\Desktop\etrun\
+xcopy /Y etrun.pk3 "C:\Program Files (x86)\ET\etrun\"
+del /Q etrun.pk3
+xcopy /Y build\*.dll C:\Users\Nicolas\Desktop\etrun\
+xcopy /Y build\timeruns.mod C:\Users\Nicolas\Desktop\etrun\
+
+xcopy /Y etrun\custommapscripts\* "C:\Users\Nicolas\Desktop\etrun\custommapscripts\"
+xcopy /Y libs\geoip\GeoIP.dat "C:\Users\Nicolas\Desktop\etrun\"
+
+echo Press ENTER to start game...
+pause
+cd ..\
+C:\Users\Nicolas\Desktop\ETDED.exe +set fs_basepath "C:\Users\Nicolas\Desktop\" +set fs_game etrun +map killua-pornstar +set developer 1 +set dedicated 1 +set g_useAPI 1 +set g_APImoduleName "timeruns.mod" +set physics 255
