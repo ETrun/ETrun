0947171b69be178166ee309e5004b3b66c74f424
Nicolas Boutet <amd3002@gmail.com>
2015-11-04 10:38:31 +0100

    game: fix cpu usage going to 100% on linux because of wrong sleep value

--- src/game/g_utils.c
+++ src/game/g_utils.c
@@ -1018,6 +1018,6 @@ void my_sleep(unsigned milliseconds) {
 #else
 # include <unistd.h>
 void my_sleep(unsigned milliseconds) {
-	sleep(milliseconds / 1000);
+	usleep(milliseconds * 1000);
 }
 #endif
