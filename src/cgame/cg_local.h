bd56ef7d55a50377d425770fe449f8b58db6aa7e
suburb <31364635+suburbski@users.noreply.github.com>
2018-06-10 16:56:39 +0200

    cgame: add velocity snapping hud (#94)
    * added snapshud for logged out only
    
    * :)
    
    * added cupmode check
    
    * indents fix
    
    * added typecast
    
    * Update cg_main.c

--- src/cgame/cg_local.h
+++ src/cgame/cg_local.h
@@ -1003,6 +1003,11 @@ typedef struct {
 	int timerunJumpCounter;
 	int timerunJumpSpeeds[256];
 
+	// suburb, Velocity Snapping
+	float snapZones[128]; // 128 being the max amount of drawn snapzones
+	float snapSpeed;
+	int snapCount;
+
 	// Accel HUD
 	float oldSpeed;
 
@@ -1771,6 +1776,12 @@ extern vmCvar_t cg_autoLogin;
 // CGaz
 extern vmCvar_t cg_drawCGaz;
 
+// Velocity Snapping
+extern vmCvar_t cg_drawVelocitySnapping;
+extern vmCvar_t cg_velocitySnappingH;
+extern vmCvar_t cg_velocitySnappingY;
+extern vmCvar_t cg_velocitySnappingFov;
+
 // Load view angles on load
 extern vmCvar_t cg_loadViewAngles;
 
@@ -1915,6 +1926,9 @@ void CG_DrawSides(float x, float y, float w, float h, float size);
 void CG_DrawTopBottom(float x, float y, float w, float h, float size);
 void CG_DrawTopBottom_NoScale(float x, float y, float w, float h, float size);
 
+// suburb, for velocity snapping
+void CG_FillAngleYaw(float start, float end, float viewangle, float y, float height, int fov, const float *color);
+
 // NERVE - SMF - localization functions
 void CG_InitTranslation(void);
 char *CG_TranslateString(const char *string);
@@ -1964,6 +1978,7 @@ void CG_DrawOB(void);
 void CG_DrawSlick(void);
 void CG_DrawTimer(void);
 void CG_DrawCGaz(void);
+void CG_DrawVelocitySnapping(void);
 void CG_DrawKeys(void);
 void CG_DrawClock(float x, float y, float scale, qboolean shadowed);
 void CG_DrawBannerPrint(void);
