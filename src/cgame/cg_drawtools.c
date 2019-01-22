66cb2d588aab2f027755f179812a44323f3687ae
suburb <31364635+suburbski@users.noreply.github.com>
2019-01-22 11:08:45 +0100

    Typo

--- src/cgame/cg_drawtools.c
+++ src/cgame/cg_drawtools.c
@@ -117,13 +117,13 @@ modified by @suburb
 */
 void CG_FillAngleYaw(float start, float end, float viewangle, float y, float height, int fov, const float *color) {
 	float x, width, fovscale;
-	
+
 	fovscale = tan(DEG2RAD(fov / 2));
 	x        = CG_WideX(SCREEN_WIDTH) / 2 + tan(DEG2RAD(viewangle + start)) / fovscale * CG_WideX(SCREEN_WIDTH) / 2;
 	width    = fabs(CG_WideX(SCREEN_WIDTH) * (tan(DEG2RAD(viewangle + end)) - tan(DEG2RAD(viewangle + start))) / (fovscale * 2)) + 1;
 	if (cg_drawVelocitySnapping.integer == 2) {
 		width /= 2;
-		if (cg.predictedPlayerState.stats[STAT_USERCMD_MOVE] & UMOVE_RIGHT;) {
+		if (cg.predictedPlayerState.stats[STAT_USERCMD_MOVE] & UMOVE_RIGHT) {
 			x += width; // invert white bars if moving to the right
 		}
 	}
