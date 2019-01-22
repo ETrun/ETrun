715ebfd72a5890e3aaf4d3aacd18469ee9c952df
suburb <31364635+suburbski@users.noreply.github.com>
2019-01-22 13:43:42 +0100

    Snapshud VQ3 shift fix (#181)
    * cgame: vq3 snapshud shift fix
    
    * Remove single used var

--- src/cgame/cg_timerun_draw.c
+++ src/cgame/cg_timerun_draw.c
@@ -732,8 +732,12 @@ void CG_DrawVelocitySnapping(void) {
 	}
 
 	// draw snapping
-	yaw = cg.predictedPlayerState.viewangles[YAW] + 45;
-
+	yaw = cg.predictedPlayerState.viewangles[YAW];
+	
+	if (physics.integer == PHYSICS_MODE_AP_NO_OB || physics.integer == PHYSICS_MODE_AP_OB) { // only shift for AP physics
+		yaw += 45;
+	}
+	
 	snapHud_H = cg_velocitySnappingH.integer;
 	snapHud_Y = cg_velocitySnappingY.integer;
 	fov       = cg_velocitySnappingFov.integer;
